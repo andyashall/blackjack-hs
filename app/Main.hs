0f683f3a9af3d83f568fee9be72fb4a5dce23866
andy <andy@ashall.co>
2017-10-13 16:22:48 +0100

    more testing

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,19 @@ main = do
   deck <- shuffleM newDeck
   let (d, dh, ph) = addCard (deck, [], [])
   let (d1, dh1, ph1) = addCard (d, ph, dh)
-  print $ map cardValue dh1
+  game (d1, dh1, ph1)
+
+game :: Game -> IO ()
+game (d, ph, dh) = do
+  if busted ph
+    then putStrLn "You busted!"
+      else if busted dh
+        then putStrLn "Dealer busted!"
+        else do
+          putStrLn $ "Dealer hand: " ++ id (show (sum $ map cardValue dh))
+          putStrLn $ "Your hand: " ++ id (show (sum $ map cardValue ph))
+          action <- getLine
+          putStrLn action
 
 newDeck :: Deck
 newDeck = concat $ replicate 4 $ map Numeric [2..10] ++ [Ace, King, Queen, Jack]
@@ -26,3 +38,6 @@ cardValue King = 10
 cardValue Queen = 10
 cardValue Jack = 10
 cardValue (Numeric i) = i 
+
+busted :: Deck -> Bool
+busted d = (sum $ map cardValue d) > 21
